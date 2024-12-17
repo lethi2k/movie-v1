ee0551af7e4def509d950fde7c7c323b1a5c8c26
Thi Le <thild@dashsolutions.com>
2024-12-17 15:34:48 +0700

    feat: add apui

--- Modules/Movie/Routes/api.php
+++ Modules/Movie/Routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Modules\Movie\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,21 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/movie', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', [MovieController::class, 'index'])->name('home.index');
+Route::get('/request', [MovieController::class, 'request'])->name('home.request');
+Route::get('/contact', [MovieController::class, 'contact'])->name('home.contact');
+Route::get('/faq', [MovieController::class, 'faq'])->name('home.faq');
+Route::get('/danh-muc', [MovieController::class, 'category'])->name('home.category');
+Route::get('/xem-phim/{slug}', [MovieController::class, 'preview'])->name('home.preview');
+Route::get('/xem-phim/{slug}/{episode}', [MovieController::class, 'show'])->name('home.detail');
+Route::get('/login', [MovieController::class, 'login'])->name('home.login');
+Route::post('/verify-account', [MovieController::class, 'verifyAccount'])->name('home.verify-account');
+Route::get('/forgot-password', [MovieController::class, 'forgotPassword'])->name('home.forgot-password');
+Route::get('/register', [MovieController::class, 'register'])->name('home.register');
+Route::post('/sign-up', [MovieController::class, 'signUp'])->name('home.sign-up');
+Route::get('/sign-out', [MovieController::class, 'signOut'])->name('home.sign-out');
+Route::fallback(function () {
+    return redirect()->route('home.index');
 });
