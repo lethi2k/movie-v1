d28cf88350b0f20994675125399dd899c30d648f
lethi2k <ledinhthi2909@gmail.com>
2024-12-20 22:22:10 +0700

    feat: update syt

--- Modules/Admin/Http/Controllers/Ecommerce/Api/ProductController.php
+++ Modules/Admin/Http/Controllers/Ecommerce/Api/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
     public function __construct(
         CategoryRepositoryInterface $categoryService,
         ProductRepositoryInterface $productService,
-        OptionRepositoryInterface $variantService,
+        OptionRepositoryInterface $variantService
     ) {
         $this->categoryService = $categoryService;
         $this->productService = $productService;
